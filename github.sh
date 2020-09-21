d086460abd388b1016cfd812c0751edf7cc55c5f
Jaimin1312 <61569981+Jaimin1312@users.noreply.github.com>
2020-09-21 17:02:21 +0530

    Update github.sh

--- github.sh
+++ github.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 git init 
-git remote add origin 'https://github.com/Jaimin1312/Ace-Editor-Theme-Django.git'
+git remote add origin 'typr url of git'
 git pull origin master
 git status 
 git add -A
 git commit -a -m "first commit"
 git log
-git branch 'Ace-Editor'
-git checkout 'Ace-Editor'
+git branch 'name'
+git checkout 'name'
 git status
-git push origin 'Ace-Editor'
+git push origin 'name'
